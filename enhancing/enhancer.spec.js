5a570f12cf8f80a1054acc11baabc2e90d1f0512
shayaanakbar <shayaanakbar1106@gmail.com>
2019-06-19 15:12:37 -0500

    Success passed

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -26,6 +26,33 @@ describe('Enhancers', () => {
       const repairSword = repair(superItem);
 
       expect(actual).toEqual(randomItem);
-    })
-  })
+    });
+  });// end of repair testing function
+
+  describe('succeed function testing', () => {
+    it('should increase item\'s enhancement by 1', () => {
+      const item = {
+        name: 'weapon',
+        enhancement: 10,
+        durability: 50
+      };
+
+      const actualSucceed = succeed(item);
+
+      expect(actualSucceed).toMatchObject({...item, enhancement: 11})
+
+    });// end of it success test
+
+    it('item enhancement should remain 20', () => {
+      const maxedEnhancement = {
+        name: 'wand',
+        enhancement: 20,
+        durability: 50
+      }
+
+      expect(succeed(maxedEnhancement)).toMatchObject({...maxedEnhancement, enhancement: 20})
+
+    });
+  });// end of success testing function
+
 })
